9d1675b2de4bdebd9ca71d854d499b75874481b7
Evandro Dutra <evandro@evandrodutra.net>
2009-02-05 17:54:16 -0200

    create base models

---
+++ db/migrate/20090128121957_twitter_users.rb
@@ -0,0 +1,18 @@
+class TwitterUsers < ActiveRecord::Migration
+	
+  def self.up
+    create_table "twitter_users", :force => true do |t|
+      t.integer :twitter_id
+      t.integer :hp, :default => 10000
+      t.integer :xp, :default => 0
+      t.references :level
+      t.string :guild
+      t.timestamps
+    end
+    add_index :users, :twitter_id, :unique => true
+  end
+
+  def self.down
+    drop_table "users"
+  end
+end
