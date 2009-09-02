ad0b30263853fa6ef0efd9383d47c83c0894da1d
a2c <a2c@a2c-laptop.(none)>
2009-09-01 22:37:52 -0300

    simple daemon

--- config/environment.rb
+++ config/environment.rb
@@ -27,6 +27,7 @@ Rails::Initializer.run do |config|
   config.gem "rubyist-aasm", :lib => "aasm"
   config.gem "paperclip", :version => '>= 2.1.2'
   config.gem 'mislav-will_paginate', :version => '~> 2.3.11', :lib => 'will_paginate'
+  config.gem "simple-daemon", :version => '>= 0.1.2'
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
